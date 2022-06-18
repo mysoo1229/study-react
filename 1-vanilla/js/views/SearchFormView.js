14b979cc1848bb962d5e379f1d9ea77e00ef4055
Crystal <crystalyoodesign@gmail.com>
2022-06-18 19:07:32 +0900

    vanilla - show or hide placeholder and reset button on input change

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,21 +1,42 @@
-import {qs} from '../helpers.js';
+import {on, qs} from '../helpers.js';
 import View from './View.js';
 
+const tag = '[SearchFormView]';
+
 export default class SearchFormView extends View {
-    constructor() {
-        super(qs('#search-form-view'));
+  constructor() {
+    super(qs('#search-form-view'));
 
-        this.resetElement = qs('[type=reset]', this.element);
-        this.placeholderElement = qs('.placeholder', this.element);
-        this.showResetButton(false);
-        this.showPlaceholder(true);
-    }
+    this.inputElement = qs('[type=text]', this.element);
+    this.resetElement = qs('[type=reset]', this.element);
+    this.placeholderElement = qs('.placeholder', this.element);
 
-    showResetButton(visible = true) {
-        this.resetElement.style.display = visible ? "block" : "none";
-    }
+    this.showResetButton(false);
+    this.showPlaceholder(true);
+    this.bindEvent();
+  }
 
-    showPlaceholder(visible = true) {
-        this.placeholderElement.style.display = visible ? "block" : "none";
-    }
+  showResetButton(visible = true) {
+    this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  showPlaceholder(visible = true) {
+    this.placeholderElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent() {
+    on(this.inputElement, "keydown", () => this.handleKeydown());
+    on(this.inputElement, "keyup", () => this.handleKeyup());
+  }
+
+  handleKeydown() {
+    const {value} = this.inputElement;
+    this.showPlaceholder(false);
+  }
+
+  handleKeyup() {
+    const {value} = this.inputElement;
+    this.showResetButton(value.length > 0);
+    this.showPlaceholder(value.length === 0);
+  }
 }
