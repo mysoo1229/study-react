1273282d01153c8c4fcc03f4132fb9d5e7858ade
Crystal <crystalyoodesign@gmail.com>
2022-06-18 23:36:28 +0900

    vanilla - show or hide result on submit or reset

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,26 +1,39 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, {searchFormView}) {
+  constructor(store, {searchFormView, searchResultView}) {
     console.log(tag);
     this.store = store;
 
     this.searchFormView = searchFormView;
+    this.searchResultView = searchResultView;
 
     this.subscribeViewEvents();
   }
 
   subscribeViewEvents() {
-    this.searchFormView.on('@submit', event => 
-      this.search(event.detail.value)
-    ).on('@reset', () => this.reset());
+    this.searchFormView
+      .on('@submit', event => this.search(event.detail.value))
+      .on('@reset', () => this.reset());
   }
 
   search(keyword) {
-    console.log(tag, keyword);
+    this.store.search(keyword);
+    this.render();
   }
 
   reset() {
-    console.log(tag, 'reset');
+    this.store.searchKeyword = '';
+    this.store.searchResult = [];
+    this.render();
+  }
+
+  render() {
+    if (this.store.searchKeyword.length > 0) {
+      this.searchResultView.show(this.store.searchResult);
+      return;
+    }
+
+    this.searchResultView.hide();
   }
 }
