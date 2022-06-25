2a65772bcccf275ce91092c9f1b1f24a654638ca
Crystal <crystalyoodesign@gmail.com>
2022-06-25 23:53:18 +0900

    react - organize render()

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -43,46 +43,49 @@ class App extends React.Component {
   }
 
   render() {
+    const searchForm = (
+      <form
+        onSubmit={event => this.handleSubmit(event)}
+        onReset={() => this.handleReset()}
+      >
+        <label className="input-search">
+          <input
+            type="text"
+            autoFocus
+            value={this.state.searchKeyword}
+            onChange={event => this.handleChangeInput(event)}
+          />
+          {this.state.searchKeyword.length == 0 && (<span className="placeholder">검색어를 입력하세요</span>)}
+        </label>
+        {this.state.searchKeyword.length > 0 && (<button type="reset" className="button-reset" aria-label="검색어 삭제"></button>)}
+      </form>
+    );
+
+    const searchResult = (
+      this.state.searchResult.length > 0 ? (
+        <ul className="result">
+          {this.state.searchResult.map(item => {
+            return (
+              <li key={item.id}>
+                <span><img src={item.imageUrl} alt="" /></span>
+                <p>{item.name}</p>
+              </li>
+            )
+          })}
+        </ul>
+      ) : (
+        <div className="empty-box">검색 결과가 없습니다</div>
+      )
+    );
+
     return (
       <>
         <header>
           <h2 className="container">검색</h2>
         </header>
         <div className="container">
-          <form
-            onSubmit={event => this.handleSubmit(event)}
-            onReset={() => this.handleReset()}
-          >
-            <label className="input-search">
-              <input
-                type="text"
-                autoFocus
-                value={this.state.searchKeyword}
-                onChange={event => this.handleChangeInput(event)}
-              />
-              {this.state.searchKeyword.length == 0 && (<span className="placeholder">검색어를 입력하세요</span>)}
-            </label>
-            {this.state.searchKeyword.length > 0 && (<button type="reset" className="button-reset" aria-label="검색어 삭제"></button>)}
-          </form>
-
-          <div className="content">
-            {this.state.submitted && (
-              this.state.searchResult.length > 0 ? (
-                <ul className="result">
-                  {this.state.searchResult.map(item => {
-                    return (
-                      <li key={item.id}>
-                        <span><img src={item.imageUrl} alt="" /></span>
-                        <p>{item.name}</p>
-                      </li>
-                    )
-                  })}
-                </ul>
-              ) : (
-                <div className="empty-box">검색 결과가 없습니다</div>
-              ) 
-            )}
-          </div>
+          {searchForm}
+          <div className="content">{this.state.submitted && searchResult}</div>
         </div>
       </>
     )
