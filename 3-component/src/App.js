9c277dcea696152bc3d96616ad6ddd13ff14c6a2
Crystal <crystalyoodesign@gmail.com>
2022-06-27 23:05:46 +0900

    component - add searchForm

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import Header from './components/Header.js';
+import SearchForm from './components/SearchForm.js';
 
 export default class App extends React.Component {
+  search(searchKeyword) {
+    console.log(searchKeyword);
+  }
+
+  removeKeyword() {
+    console.log('removed');
+  }
+
   render() {
-    return <Header title="검색" />;
+    return (
+      <>
+        <Header title="검색" />
+        <div className="container">
+          <SearchForm
+            onSubmit={(searchKeyword) => this.search(searchKeyword)}
+            onReset={() => this.removeKeyword()}
+          />
+        </div>
+      </>
+    );
   }
 }
