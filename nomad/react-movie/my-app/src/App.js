bfb9ba41bc6d78f8fa762d14a9852633a09e6e32
Crystal <crystalyoodesign@gmail.com>
2023-02-19 12:52:18 +0900

    #6 Effects - useEffect and Deps

--- nomad/react-movie/my-app/src/App.js
+++ nomad/react-movie/my-app/src/App.js
@@ -1,11 +1,41 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue(prev => prev + 1);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+
+  // console.log('i run all the time');
+
+  useEffect(() => {
+    console.log('i run only once (api)')
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "") {
+      console.log('i run when KEYWORD changes');
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log('i run when COUNTER changes')
+  }, [counter]);
+
+  useEffect(() => {
+    console.log('i run when KEYWORD & COUNTER changes')
+  }, [keyword, counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
