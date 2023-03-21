37e7f02c7f17e9f877779ee5d64788d3b35287d9
Crystal <crystalyoodesign@gmail.com>
2023-03-21 23:30:44 +0900

    #2  Styled Components - Animations and Pseudo Selectors

--- nomad/react-masterclass/src/App.js
+++ nomad/react-masterclass/src/App.js
@@ -1,31 +1,50 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Btn = styled.button`
-  color: white;
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0;
+  }
+
+  50% {
+    border-radius: 100px;
+  }
+
+  0% {
+    transform: rotate(360deg);
+    border-radius: 0;
+  }
 `;
 
-const Input = styled.input.attrs({required: true, minLength: 10})`
-  background-color: #c9c;
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
+
+  span {
+    font-size: 36px;
+
+    &:hover {
+      font-size: 60px;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Btn>Log in</Btn>
-      <Btn as="a" href="/">Log in</Btn>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🤮</span>
+      </Box>
+    </Wrapper>
   );
 }
 
