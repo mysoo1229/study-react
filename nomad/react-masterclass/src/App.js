e78b244b05141e7ab154f2243c37f9606894566b
Crystal <crystalyoodesign@gmail.com>
2023-03-22 22:32:22 +0900

    #2  Styled Components - Themes

--- nomad/react-masterclass/src/App.js
+++ nomad/react-masterclass/src/App.js
@@ -1,56 +1,20 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
   width: 100vw;
   height: 100vh;
-  justify-content: center;
-  align-items: center;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  }
-
-  50% {
-    border-radius: 100px;
-  }
-
-  0% {
-    transform: rotate(360deg);
-    border-radius: 0;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 200px;
-  height: 200px;
-  background: tomato;
-  animation: ${rotationAnimation} 1s linear infinite;
-
-  ${Emoji} {
-    &:hover {
-      font-size: 60px;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😵‍💫</Emoji>
-      </Box>
-      <Emoji>🤮</Emoji>
+      <Title>This is my title.</Title>
     </Wrapper>
   );
 }
