82a4757167ada74105ea7d7bda3871b62915af69
Crystal <crystalyoodesign@gmail.com>
2023-04-11 21:58:50 +0900

    #5 Crypto Tracker

--- nomad/react-masterclass/my-typescript/src/App.tsx
+++ nomad/react-masterclass/my-typescript/src/App.tsx
@@ -1,20 +1,8 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props)=> props.theme.textColor};
-`;
+import Router from "./Router";
 
 function App() {
   return (
-    <Container>
-      <H1>Protected</H1>
-    </Container>
+    <Router />
   );
 }
 
