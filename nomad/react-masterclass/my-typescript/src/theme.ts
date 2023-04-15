e609e1c1d5876da83e9505d969a698c06420a2d4
Crystal <crystalyoodesign@gmail.com>
2023-04-16 01:03:33 +0900

    #5 Crypto Tracker - Styles

--- nomad/react-masterclass/my-typescript/src/theme.ts
+++ nomad/react-masterclass/my-typescript/src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#222",
+  textColor: "#f3f3f3",
+  accentColor: "#4692ff"
 };
