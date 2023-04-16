c07b2077cce0e3827eb42c371c0e40090d568021
Crystal <crystalyoodesign@gmail.com>
2023-04-16 19:30:12 +0900

    #5 Crypto Tracker - Home Part One

--- nomad/react-masterclass/my-typescript/src/index.tsx
+++ nomad/react-masterclass/my-typescript/src/index.tsx
@@ -8,9 +8,9 @@ window.React = React;
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.StrictMode>
+  <div>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </div>
 );
--- nomad/react-masterclass/my-typescript/src/routes/Coins.tsx
+++ nomad/react-masterclass/my-typescript/src/routes/Coins.tsx
@@ -1,11 +1,89 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  margin-bottom: 12px;
+  border-radius: 8px;
+  background-color: white;
+  color: ${props => props.theme.bgColor};
+
+  a {
+    display: block;
+    padding: 12px 16px;
+    transition: color .2s ease;
+  }
+
+  &:hover {
+    a {
+      color: ${props => props.theme.accentColor};
+    }
+  }
+`;
+
 const Title = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
   color: ${props => props.theme.accentColor};
 `;
 
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 function Coins() {
-  return <Title>Coins</Title>
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinsList>
+        {coins.map(coin => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>)
+        )}
+      </CoinsList>
+    </Container>
+  )
 }
 
 export default Coins;
