dbe5b525157667dca2167917350da20b5919cd36
Crystal <crystalyoodesign@gmail.com>
2023-04-30 16:51:34 +0900

    #5 Crypto Tracker - Price Chart part One

--- nomad/react-masterclass/my-typescript/src/routes/Chart.tsx
+++ nomad/react-masterclass/my-typescript/src/routes/Chart.tsx
@@ -1,4 +1,16 @@
-function Chart() {
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId}: ChartProps) {
+  // const params = useParams();
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+
   return <h1>Chart</h1>;
 }
 
