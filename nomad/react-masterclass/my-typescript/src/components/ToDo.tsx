f975c467a4c287900bbec8ec19c04e7c4f2e6dd5
Crystal <crystalyoodesign@gmail.com>
2023-05-14 22:30:53 +0900

    #6 To Do - Refactoring

---
+++ nomad/react-masterclass/my-typescript/src/components/ToDo.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { IToDo } from "../atoms";
+
+function ToDo({text}: IToDo) {
+  return (
+    <li>
+      <span>{text}</span>
+      <button>To Do</button>
+      <button>Doing</button>
+      <button>Done</button>
+    </li>
+  );
+}
+
+export default ToDo;
--- nomad/react-masterclass/my-typescript/src/components/ToDoList.tsx
+++ nomad/react-masterclass/my-typescript/src/components/ToDoList.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { atom, useRecoilState } from "recoil";
+import React from "react";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 
 const Container = styled.div`
   padding: 20px;
@@ -15,52 +17,19 @@ const ResultList = styled.ul`
   margin-top: 30px;
 `;
 
-interface IUseForm {
-  toDo: string;
-};
-
-interface IToDo {
-  id: number;
-  text: string;
-  category: "TO_DO" | "DOING" | "DONE";
-}
-
-const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
-
 function ToDoList() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-
-  // const toDos = useRecoilValue(toDoState); //get atom value
-  // const setToDos = useSetRecoilState(toDoState); //update atom value (modifier)
-
-  const { register, handleSubmit, setValue } = useForm<IUseForm>();
-
-  const handleValid = ({toDo}: IUseForm) => {
-    setToDos(oldToDos => [
-      { id: Date.now(), text: toDo, category: "TO_DO" },
-      ...oldToDos
-    ]);
-    setValue("toDo", "");
-  };
+  const toDos = useRecoilValue(toDoState);
 
   return (
     <Container>
       <H1>To-Dos</H1>
-      <form onSubmit={handleSubmit(handleValid)}>
-        <input
-          {...register("toDo", {
-            required: "Please write a To Do",
-          })}
-          placeholder="Write a to do"
-        />
-        <button>Add</button>
-      </form>
+      <CreateToDo />
       <ResultList>
         {toDos.map(toDo => (
-          <li key={toDo.id}>{toDo.text}</li>
+          <ToDo
+            key={toDo.id}  
+            {...toDo}
+          />
         ))}
       </ResultList>
     </Container>
