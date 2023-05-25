d58da2063ea6f700084f04fb19983f147e9cd5cc
Crystal <crystalyoodesign@gmail.com>
2023-05-25 22:25:20 +0900

    #6 To Do - Selectors part One

--- nomad/react-masterclass/my-typescript/src/components/ToDoList.tsx
+++ nomad/react-masterclass/my-typescript/src/components/ToDoList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -12,25 +12,42 @@ const H1 = styled.h1`
   margin-bottom: 20px;
   font-size: 26px;
 `;
+const H2 = styled.h2`
+  margin-top: 20px;
+  font-size: 22px;
+`;
 
 const ResultList = styled.ul`
   margin-top: 30px;
 `;
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  // const toDos = useRecoilValue(toDoState);
+  // const selectorOutput = useRecoilValue(toDoSelector);
+  // console.log(selectorOutput);
+
+  const [toDo, doing, done] = useRecoilValue(toDoSelector)
 
   return (
     <Container>
       <H1>To-Dos</H1>
       <CreateToDo />
+      <H2>To Do</H2>
       <ResultList>
-        {toDos.map(toDo => (
-          <ToDo
-            key={toDo.id}  
-            {...toDo}
-          />
+        {toDo.map(toDo => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ResultList>
+      <H2>Doing</H2>
+      <ResultList>
+        {doing.map(toDo => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ResultList>
+      <H2>DONE</H2>
+      <ResultList>
+        {done.map(toDo => (
+          <ToDo key={toDo.id} {...toDo} />
         ))}
       </ResultList>
     </Container>
