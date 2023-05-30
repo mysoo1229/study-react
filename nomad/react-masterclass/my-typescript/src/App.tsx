3065f56f324d0d88276fb3b21178ba146cf9ab09
Crystal <crystalyoo29@interpark.com>
2023-05-30 17:44:09 +0900

    #7 Practice - Get Selectors

--- nomad/react-masterclass/my-typescript/src/App.tsx
+++ nomad/react-masterclass/my-typescript/src/App.tsx
@@ -1,7 +1,28 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
+import React from "react";
 
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value); //+ sign turns the string into a number
+  };
+
   return (
     <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input
+        value={hours}
+        type="number"
+        placeholder="Hours"
+      />
     </div>
   );
 }
