c9fb6d2fe0f2d804f0720c72b69b4001e352f276
Crystal <crystalyoo29@interpark.com>
2023-05-30 17:32:35 +0900

    cleanup

--- nomad/react-masterclass/my-typescript/src/theme.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultTheme } from "styled-components";
-
-export const darkTheme: DefaultTheme = {
-  bgColor: "#222",
-  fillColor: "#000",
-  textColor: "#f3f3f3",
-  accentColor: "#4692ff"
-};
