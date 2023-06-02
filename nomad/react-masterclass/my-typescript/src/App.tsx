bf7539c4b117e30e6368cb10d4189ff931ac191f
Crystal <crystalyoodesign@gmail.com>
2023-06-02 22:14:34 +0900

    #7 Practice - Set Selectors

--- nomad/react-masterclass/my-typescript/src/App.tsx
+++ nomad/react-masterclass/my-typescript/src/App.tsx
@@ -4,10 +4,14 @@ import React from "react";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
 
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value); //+ sign turns the string into a number
+  };
+
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
   };
 
   return (
@@ -20,6 +24,7 @@ function App() {
       />
       <input
         value={hours}
+        onChange={onHoursChange}
         type="number"
         placeholder="Hours"
       />
