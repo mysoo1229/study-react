8095a5ee0e531f132778e4f5898b904de6f3e61a
Crystal <crystalyoodesign@gmail.com>
2023-06-02 22:22:43 +0900

    cleanup

--- nomad/react-masterclass/my-typescript/src/App.tsx
+++ nomad/react-masterclass/my-typescript/src/App.tsx
@@ -1,34 +1,6 @@
-import { useRecoilState, useRecoilValue } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
-import React from "react";
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-
-  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value); //+ sign turns the string into a number
-  };
-
-  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };
-
   return (
-    <div>
-      <input
-        value={minutes}
-        onChange={onMinutesChange}
-        type="number"
-        placeholder="Minutes"
-      />
-      <input
-        value={hours}
-        onChange={onHoursChange}
-        type="number"
-        placeholder="Hours"
-      />
-    </div>
   );
 }
 
