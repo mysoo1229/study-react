277117ea1784af793861c60f8dc6e9bab218e1cd
Crystal <crystalyoodesign@gmail.com>
2023-06-17 17:04:20 +0900

    #8 Animations - Variants part Two

--- nomad/react-masterclass/my-typescript/src/App.tsx
+++ nomad/react-masterclass/my-typescript/src/App.tsx
@@ -35,9 +35,48 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, .1), 0 10px 20px rgba(0, 0, 0, .06);
 `;
 
-const myVars = {
+const BoxTranparent = styled(Box)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding: 5px;
+  background: rgba(255, 255, 255, .2);
+`;
+
+const Circle = styled(motion.div)`
+  width: 35px;
+  height: 35px;
+  place-self: center;
+  background: #fff;
+  border-radius: 50%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, .1), 0 10px 20px rgba(0, 0, 0, .06);
+`;
+
+const boxVar1 = {
   start: {scale: 0},
   end: {scale: 1, rotateZ: 360, transition: {type: "spring", delay: .5}},
+};
+
+const boxVar2 = {
+  start: {
+    opacity: 0,
+    scale: .5
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: .5,
+      bounce: .5,
+      delayChildren: .5,
+      staggerChildren: .15,
+    }
+  },
+};
+
+const circleVar = {
+  start: {opacity: 0, y: 10},
+  end: {opacity: 1, y: 0, transition: {duration: .2}},
 };
 
 function App() {
@@ -45,11 +84,18 @@ function App() {
       <Wrapper>
         <Item>
           <Title>Animation</Title>
-          <Box
-            variants={myVars}
-            initial="start"
-            animate="end"
-          />
+          <Box variants={boxVar1} initial="start" animate="end" />
+        </Item>
+
+        <Item>
+          <Title>Variant</Title>
+          <BoxTranparent variants={boxVar2} initial="start" animate="end">
+            {/* <Circle initial="start" animate="end" /> this is default */}
+            <Circle variants={circleVar} />
+            <Circle variants={circleVar} />
+            <Circle variants={circleVar} />
+            <Circle variants={circleVar} />
+          </BoxTranparent>
         </Item>
       </Wrapper>
   );
