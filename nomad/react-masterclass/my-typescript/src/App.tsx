38726173e7ff29b9a2117752f5f6ce45da335c19
Crystal <crystalyoodesign@gmail.com>
2023-06-17 16:18:40 +0900

    #8 Animations - Basic Animations

--- nomad/react-masterclass/my-typescript/src/App.tsx
+++ nomad/react-masterclass/my-typescript/src/App.tsx
@@ -2,26 +2,48 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 100px;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 100vh;
 `;
 
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 30px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #fff;
+`;
+
+const Box = styled(motion.div)`
+  width: 100px;
+  height: 100px;
   background: #fff;
-  border-radius: 10px;
+  border-radius: 16px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, .1), 0 10px 20px rgba(0, 0, 0, .06);
 `;
 
 function App() {
   return (
       <Wrapper>
-        <Box />
-        <motion.div></motion.div>
+        <Item>
+          <Title>Animation</Title>
+          <Box
+            transition={{type: "spring", delay: .5}}
+            initial={{scale: 0}}
+            animate={{scale: 1, rotateZ: 360}}
+          />
+        </Item>
       </Wrapper>
   );
 }
