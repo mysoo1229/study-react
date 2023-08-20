c7baf5d6ad676f339671e7c00f2654efc363f3bd
Crystal <crystalyoodesign@gmail.com>
2023-08-20 12:47:03 +0900

    #0 Hooks - Init

--- nomad/react-masterclass/my-javascript/src/App.js
+++ nomad/react-masterclass/my-javascript/src/App.js
@@ -1,22 +1,5 @@
-import styled, { keyframes } from "styled-components";
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
 function App() {
-  return (
-    <Wrapper>
-      <Title>This is my title.</Title>
-    </Wrapper>
-  );
+  return null;
 }
 
 export default App;
