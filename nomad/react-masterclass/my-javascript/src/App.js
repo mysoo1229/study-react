73cc93f3cc52a89f12fe0054918572c8ad6cadbb
Crystal <crystalyoodesign@gmail.com>
2023-08-20 13:04:41 +0900

    #1.1 useInput part One

--- nomad/react-masterclass/my-javascript/src/App.js
+++ nomad/react-masterclass/my-javascript/src/App.js
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
+const useInput = (initialVlaue) => {
+  const [value, setValue] = useState(initialVlaue);
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    setValue(value);
+  }
+  return { value, onChange };
+};
+
 function App() {
-  return null;
+  const name = useInput("Ms.");
+  return (
+    <div className="App">
+      <h1>Hooks</h1>
+      {/* <input placeholder="Name" value={name.value} onChange={namer.onChange} /> */}
+      <input placeholder="Name" {...name} />
+    </div>
+  );
 }
 
 export default App;
