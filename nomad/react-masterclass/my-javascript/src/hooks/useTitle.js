56473add7c93a9a0e67428c17e09bcda6bc4bb3d
Crystal <crystalyoodesign@gmail.com>
2023-08-20 21:27:51 +0900

    #2.1 useTitle

--- nomad/react-masterclass/my-javascript/src/hooks/useTitle.js
+++ nomad/react-masterclass/my-javascript/src/hooks/useTitle.js
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-
 const useTitle = (initialTitle) => {
   const [title, setTitle] = useState(initialTitle);
   const updateTitle  = () => {
@@ -9,16 +7,3 @@ const useTitle = (initialTitle) => {
   useEffect(updateTitle, [title]);
   return setTitle;
 };
-
-function App() {
-  const titleUpdator = useTitle("Loading...");
-  setTimeout(() => titleUpdator("Home"), 3000);
-
-  return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
-  );
-}
-
-export default App;
