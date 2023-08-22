9f93a49eb73b87889da80b8eccadecc6be579828
Crystal <crystalyoo29@interpark.com>
2023-08-22 09:48:00 +0900

    #2.3 usePreventLeave

--- nomad/react-masterclass/my-javascript/src/App.js
+++ nomad/react-masterclass/my-javascript/src/App.js
@@ -1,30 +1,20 @@
-const useConfirm = (message = "", onConfirm, onCancel) => {
-  if (!onConfirm || typeof onConfirm !== "function") {
-    return;
+const usePreventLeave = () => {
+  const listener = (event) =>{
+    event.preventDefault();
+    event.returnValue = ""; //for chrome
   }
+  const enablePrevent = () => window.addEventListener("beforeunload", listener); //beforunload: 윈도우 닫히기 전에 하는 일
+  const disablePrevent = () => window.removeEventListener("beforeunload", listener);
 
-  if (onCancel && typeof onCancel !== "function") {
-    return;
-  }
-
-  const confirmAction = () => {
-    if (window.confirm(message)) {
-      onConfirm();
-    } else {
-      onCancel();
-    }
-  }
-
-  return confirmAction;
+  return { enablePrevent, disablePrevent };
 };
 
 const App = () => {
-  const deleteWorld = () => console.log("bye world");
-  const abort = () => console.log("nope just kidding");
-  const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort);
+  const { enablePrevent, disablePrevent } = usePreventLeave();
   return (
     <div className="App">
-      <button onClick={confirmDelete}>Delete the world</button>
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>Unprotect</button>
     </div>
   );
 }
