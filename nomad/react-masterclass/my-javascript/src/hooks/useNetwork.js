ed6fb67905ae1e0401b8d2ddca6c528b494664a5
Crystal <crystalyoo29@interpark.com>
2023-08-23 10:32:32 +0900

    #2.5 useNetwork

--- nomad/react-masterclass/my-javascript/src/hooks/useNetwork.js
+++ nomad/react-masterclass/my-javascript/src/hooks/useNetwork.js
@@ -1,8 +1,5 @@
-import { useEffect, useState } from "react";
-
 const useNetwork = (onchange) => {
   const [status, setStatus] = useState(navigator.onLine);
-  //navigator.onLine : is a booelan to check if online/offline
 
   const handleChange = () => {
     setStatus(navigator.onLine);
@@ -24,18 +21,3 @@ const useNetwork = (onchange) => {
 
   return status;
 };
-
-const App = () => {
-  const handleNetworkChange = (online) => {
-    console.log(online ? "We are online" : "We are offline");
-  };
-  const onLine = useNetwork(handleNetworkChange);
-
-  return (
-    <div className="App">
-      <h1>{onLine ? "onLine" : "offLine"}</h1>
-    </div>
-  );
-};
-
-export default App;
