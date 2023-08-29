5bff9f4b98cb49df3cd6895723c0c46f5455e855
Crystal <crystalyoo29@interpark.com>
2023-08-29 11:15:11 +0900

    #1.3 Routing

--- nomad/nextjs-intro/pages/about.js
+++ nomad/nextjs-intro/pages/about.js
@@ -1,3 +1,10 @@
+import NavBar from "@/components/NavBar";
+
 export default function About() {
-  return "About Us";
+  return (
+    <div>
+      <NavBar />
+      <h1>About US</h1>
+    </div>
+  )
 }
