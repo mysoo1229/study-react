080f8ad633cfbc142554a6e35eeb7de2509a11a6
Crystal <crystalyoo29@interpark.com>
2023-08-29 11:23:17 +0900

    #1.4 CSS Modules

--- nomad/nextjs-intro/components/NavBar.js
+++ nomad/nextjs-intro/components/NavBar.js
@@ -1,14 +1,24 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
-  console.log(router);
 
   return (
-    <nav>
-      <Link style={{color: router.pathname === "/" ? "red" : "black"}} href="/">Home</Link>
-      <Link style={{color: router.pathname === "/about" ? "red" : "black"}} href="/about">About</Link>
+    <nav className={styles.nav}>
+      <Link
+        href="/"
+        className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}
+      >
+        Home
+      </Link>
+      <Link
+        href="/about"
+        className={[styles.link, router.pathname === "/about" ? styles.active : ""].join(' ')}
+      >
+        About
+      </Link>
     </nav>
   );
 }
