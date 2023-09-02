1ca2e7e25080c172fe04e9a0b3cd2d93bc948379
Crystal <crystalyoodesign@gmail.com>
2023-09-02 21:59:26 +0900

    #2.0 Patterns

--- nomad/nextjs-intro/pages/_app.js
+++ nomad/nextjs-intro/pages/_app.js
@@ -1,10 +1,10 @@
 import "../styles/globals.css";
+import Layout from "@/components/Layout";
 
 export default function App({Component, pageProps}) {
   return (
-    <div>
+    <Layout>
       <Component {...pageProps} />
-      <span>Wow</span>
-    </div>
+    </Layout>
   )
 }
--- nomad/nextjs-intro/pages/about.js
+++ nomad/nextjs-intro/pages/about.js
@@ -1,9 +1,9 @@
-import NavBar from "@/components/NavBar";
+import Seo from "@/components/Seo";
 
 export default function About() {
   return (
     <div>
-      <NavBar />
+      <Seo title="About" />
       <h1>About US</h1>
     </div>
   )
