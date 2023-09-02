5823dbdd5fed7c1921e0bf9aad389faa203ebad8
Crystal <crystalyoodesign@gmail.com>
2023-09-02 15:41:04 +0900

    #1.5 Styles JSX

--- nomad/nextjs-intro/components/NavBar.js
+++ nomad/nextjs-intro/components/NavBar.js
@@ -1,24 +1,38 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
 
   return (
-    <nav className={styles.nav}>
+    <nav>
       <Link
         href="/"
-        className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}
+        className={router.pathname === "/" ? "active" : ""}
       >
         Home
       </Link>
       <Link
         href="/about"
-        className={[styles.link, router.pathname === "/about" ? styles.active : ""].join(' ')}
+        className={router.pathname === "/about" ? "active" : ""}
       >
         About
       </Link>
+
+      <style jsx global>
+        {`
+          nav {
+            background-color: cornflowerblue;
+          }
+          a {
+            text-decoration: none;
+            color: #fff;
+          }
+          .active {
+            color: red;
+          }
+        `}
+      </style>
     </nav>
   );
 }
