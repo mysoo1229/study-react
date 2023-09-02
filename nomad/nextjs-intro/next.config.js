edbacacf891e00d56d21e163e64748f99a3aacf3
Crystal <crystalyoodesign@gmail.com>
2023-09-02 23:07:11 +0900

    #2.2 Redirect and Rewrite

--- nomad/nextjs-intro/next.config.js
+++ nomad/nextjs-intro/next.config.js
@@ -1,6 +1,28 @@
+const API_KEY = "6c61618f0f7e88b0c9c7babe5058cf8f";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/learn",
+        destination: "https://nomadcoders.co",
+        permanent: false,
+      },
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      }
+    ];
+  },
+  async rewrites() {
+    return [{
+      source: "/api/movies",
+      destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+    }];
+  },
 }
 
 module.exports = nextConfig
